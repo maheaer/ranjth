12c70b466c39135181e893d15794f7e89406c7c2
mahesh <maheaer30@gmail.com>
2018-05-10 10:17:40 +0530

    initial commit

---
+++ ranjth/src/main/java/ranjth/app.java
@@ -0,0 +1,8 @@
+package ranjth;
+
+public class app {
+
+	public String sample() {
+		return "Sample Program";
+	}
+}
---
+++ ranjth/src/test/java/ranjth/apptest.java
@@ -0,0 +1,8 @@
+package ranjth;
+
+public class apptest {
+	public static void tearDownafterclass() throws Exception {	
+	}
+	public void test() {
+}
+}
